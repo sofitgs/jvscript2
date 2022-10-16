4bd3cb45a3d751897a92519a9ee4cfb595a4c899
Sofia Gordillo <sofia.tamara31@gmail.com>
2022-10-15 19:47:55 -0500

    agregue funciones

--- javascript/prueba1.js
+++ javascript/prueba1.js
@@ -1,7 +1,9 @@
-let nombre = prompt("Ingresa tu nombre completo")
-let empresa = prompt("Ingrese el nombre de su empresa")
-let area = prompt("Ingrese a que área pertenece")
 
+let nombre = prompt("Ingresa tu nombre completo")
+//let empresa = prompt("Ingrese el nombre de su empresa")
+//let area = prompt("Ingrese a que área pertenece")
+
+/*
 if (empresa === "Coderhouse") {
     console.log("Bienvenido Coder")
 } else {
@@ -33,4 +35,39 @@ if (equipo === true) {
             break
     }
 
+} */
+
+function superpoder (){
+    console.log("Hola", nombre, "tu superpoder es la creatividad")
 }
+
+ superpoder ()
+
+ function estrellas (){
+    let accion1 = parseFloat(prompt("Ingresa cuantas veces ayudaste a otra área"))
+    let accion2 = parseFloat(prompt("Ingresa cuantos proyectos lideraste"))
+    let accion3 = parseFloat(prompt("Ingresa a cuantos compañeros les diste estrellas"))
+    let resultado = accion1 + (accion2 * 2) + (accion3 * 3)
+
+    console.log("Tu ranking estrellado es", resultado)
+}
+
+estrellas ()
+
+const ranking = ["Juan", "Maria", "Alejandro", "Sofia", "Patricia"]
+
+function listarRanking (){
+    console.table(ranking)
+}
+
+listarRanking ()
+
+function filtrarRanking (){
+    let buscar = prompt("Ingresa el nombre que crees es top 5 estrellado")
+    let resultado1 = ranking.filter(elemento => elemento.ranking === buscar)    
+    if (resultado1 === undefined){
+        console.warn("No es parte del top 5 estrellado aún")
+    } else {console.table (resultado1)}
+}
+
+filtrarRanking()
